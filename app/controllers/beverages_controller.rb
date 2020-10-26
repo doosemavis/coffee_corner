aacc2be7e5f0557abe5d2a584bcc9fc4a2ea077f
doosemavis <moosedavis2011@gmail.com>
2020-10-26 13:29:49 -0500

    added controllers, associations, views

---
+++ app/controllers/beverages_controller.rb
@@ -0,0 +1,3 @@
+class BeveragesController < ApplicationController
+
+end
---
+++ app/controllers/roasters_controller.rb
@@ -0,0 +1,7 @@
+class RoastersController < ApplicationController
+
+    get "/" do
+        erb :'roaster/index'
+    end
+
+end
