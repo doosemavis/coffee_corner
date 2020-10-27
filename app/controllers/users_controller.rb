dfa1eda050328bf969e05744a1b43cc039e51856
doosemavis <moosedavis2011@gmail.com>
2020-10-27 12:25:32 -0500

    signup and welcome page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,19 @@ class UsersController < ApplicationController
     end
 
     post '/login' do
-        user = User.create(username: params[:username], password: params[:password])
+        user = User.create(username: params[:username], 
+        password: params[:password])
     end
 
     get '/signup' do 
         erb :'user/signup'
+    end
+
+    post '/signup' do 
+        user = User.create(name: params[:name], 
+        email: params[:email], 
+        username: params[:username], 
+        password: params[:password])
     end 
 
 end
