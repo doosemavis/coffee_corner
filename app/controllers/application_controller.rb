8a507a958675938690c4e3dfc8ce4ecf734a776e
doosemavis <moosedavis2011@gmail.com>
2020-10-27 16:47:37 -0500

    display users roaster info

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < Sinatra::Base
     end
     
     def current_user
-      @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+      @user ||= User.find_by_id(session[:user_id]) if session[:user_id]
     end
   end
 
