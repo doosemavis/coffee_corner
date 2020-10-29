78dc001d95aac052fcd1a8039a9abdb4b23d9365
doosemavis <moosedavis2011@gmail.com>
2020-10-29 08:20:39 -0500

    small validation edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
     has_secure_password
     has_many :roasters
     has_many :beverages, through: :roasters
-    validates :name, :username, :password, :email, presence: true
+    validates :username, :password, presence: true
 end
