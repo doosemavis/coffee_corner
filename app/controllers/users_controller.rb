b4d1b02a30c1e70faa8933f38eae95b86c3ecde3
doosemavis <moosedavis2011@gmail.com>
2020-11-06 14:21:25 -0600

    Deleted commented out data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,33 +11,12 @@ class UsersController < ApplicationController
         password: params[:password])
         binding.pry
 
-        if user 
+        if user.valid?
             session[:user_id] = user.id
             redirect '/roasters'
         else 
             redirect '/login'
         end
     end
-
-
-    # get '/login' do
-    #     erb :'users/login'
-    # end 
-
-    # post '/login' do
-    #     @user = User.find_by(username: params[:username])
-    #     # binding.pry
-    #     if @user && @user.authenticate(params[:password])
-    #         session[:user_id] = @user.id
-    #         redirect '/roasters'
-    #     else 
-    #         redirect '/signup'
-    #     end
-    # end
-
-    # get '/logout' do
-    #     session.destroy
-    #     redirect '/login'
-    # end
     
 end
